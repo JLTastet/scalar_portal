0738317b7a79f67fbdde2f5c21782ee55b9ef3a2
Jean-Loup Tastet <jeanloup@nbi.ku.dk>
2019-01-22 17:15:01 +0100

    Let's not test decays with a massless scalar... It makes no sense.

--- test/test_decay.py
+++ test/test_decay.py
@@ -8,9 +8,10 @@ import numpy as np
 from ..data.particles import *
 from ..decay import leptonic as lp
 from ..decay import two_pions as tp
+from ..decay import two_gluons as gg
 
 def test_leptonic_width():
-    mS = np.array([0, 0.1, 0.5, 1.0, 2.0, 4.0, 10.0])
+    mS = np.array([0.01, 0.1, 0.5, 1.0, 2.0, 4.0, 10.0])
     w = lp.normalized_decay_width('e'  , mS)
     assert(all(w[mS > 2*get_mass('e'  )] >  0))
     assert(all(w[mS < 2*get_mass('e'  )] == 0))
@@ -23,7 +24,7 @@ def test_leptonic_width():
     assert_raises(ValueError, lambda: lp.normalized_decay_width('K', mS))
 
 def test_two_pion_width():
-    mS = np.array([0, 0.1, 0.2, 0.3, 0.5, 0.8, 1.0, 1.2, 2.0, 4.0, 10.0])
+    mS = np.array([0.01, 0.1, 0.2, 0.3, 0.5, 0.8, 1.0, 1.2, 2.0, 4.0, 10.0])
     w = tp.normalized_decay_width('neutral', mS)
     assert(all(w[mS < 2*get_mass('pi')]) == 0)
     assert(all(w[np.logical_and(mS > 2*get_mass('pi'), mS < 1.0)]) > 0)
