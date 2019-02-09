9506c0531f16da08979d3ed94e42d1fa3451433c
Jean-Loup Tastet <jeanloup@nbi.ku.dk>
2019-02-09 13:00:24 +0100

    Fix leptonic decay width (S -> ll) and validate it.
    * Replace wrong '/' by '*'.
    * Validate results against a Mathematica implementation.
    * Write unit-tests to compare against the validation targets.

--- decay/leptonic.py
+++ decay/leptonic.py
@@ -14,6 +14,6 @@ def normalized_decay_width(l, mS):
         raise(ValueError('{} must be a lepton.'.format(l)))
     ml = get_mass(l)
     with np.errstate(invalid='ignore', divide='ignore'):
-        w = ( (ml**2 * mS) / (8*pi * v**2) ) / ( 1 - 4*ml**2/mS**2 )**(3/2)
+        w = ( (ml**2 * mS) / (8*pi * v**2) ) * ( 1 - 4*ml**2/mS**2 )**(3/2)
     threshold = 2*ml
     return np.where(mS > threshold, w, 0.)
--- test/test_decay.py
+++ test/test_decay.py
@@ -6,23 +6,35 @@ from nose.tools import assert_equals, assert_raises
 import numpy as np
 
 from ..data.particles import *
-from ..data import qcd_rg as rg
 from ..decay import leptonic as lp
 from ..decay import two_pions as tp
 from ..decay import two_gluons as gg
 from ..decay import two_quarks as qq
 
 def test_leptonic_width():
+    eps = 1e-12
     mS = np.array([0.01, 0.1, 0.5, 1.0, 2.0, 4.0, 10.0])
     w = lp.normalized_decay_width('e'  , mS)
     assert(all(w[mS > 2*get_mass('e'  )] >  0))
     assert(all(w[mS < 2*get_mass('e'  )] == 0))
+    target = np.array([
+        1.6900126864964634e-15, 1.7165714271571945e-14, 8.584148208244031e-14,
+        1.7168377110602744e-13, 3.433679456830234e-13, 6.867360931015804e-13,
+        1.7168403739688331e-12])
+    assert(np.all(np.abs(w - target) <= eps * target))
     w = lp.normalized_decay_width('mu' , mS)
     assert(all(w[mS > 2*get_mass('mu' )] >  0))
     assert(all(w[mS < 2*get_mass('mu' )] == 0))
+    target = np.array([
+        0, 0, 2.732025043654036e-9, 6.853907708050072e-9, 1.443491869260822e-8,
+        2.9237286606176158e-8, 7.335112420583204e-8])
+    assert(np.all(np.abs(w - target) <= eps * target))
     w = lp.normalized_decay_width('tau', mS)
     assert(all(w[mS > 2*get_mass('tau')] >  0))
     assert(all(w[mS < 2*get_mass('tau')] == 0))
+    target = np.array([
+        0, 0, 0, 0, 0, 8.028575393027576e-7, 0.00001695364846943955])
+    assert(np.all(np.abs(w - target) <= eps * target))
     assert_raises(ValueError, lambda: lp.normalized_decay_width('K', mS))
 
 def test_two_pion_width():
